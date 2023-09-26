1d300ce70f7b7de94200eff8223d399536773e65
irensmile <curkan.ira@gmail.com>
2023-09-26 08:45:50 -0400

    Use DOM navigation. Added task #10

--- js/task-10.js
+++ js/task-10.js
@@ -3,6 +3,46 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+/*const parent = document.querySelector("#parent");
+const child = document.querySelector("#child");
+const descendant = document.querySelector("#descendant");
+
+parent.addEventListener("click", () => {
+  console.log("Parent click handler");
+});
+
+child.addEventListener("click", () => {
+  console.log("Child click handler");
+});
+
+descendant.addEventListener("click", () => {
+  console.log("Descendant click handler");
+});*/
+
 function createBoxes(amount) {
-  
+  const boxesContainer = document.getElementById("boxes");
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
 }
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+}
+
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const input = document.querySelector("input");
+
+createButton.addEventListener("click", () => {
+  const amount = input.value;
+  createBoxes(amount);
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
